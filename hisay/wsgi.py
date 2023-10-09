70e64e085c982c9eae1898e1df8bf813d9037d69
Sergey <sergey.01.31.93@gmail.com>
2023-10-09 16:10:37 +0500

    added .env files configuration

--- hisay/wsgi.py
+++ hisay/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hisay.settings')
 
+project_folder = os.path.expanduser('~/api_hisay')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
