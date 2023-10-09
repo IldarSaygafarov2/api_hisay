9a40afde8ec9e2db6c95c21de9b8034666a7a2c1
Sergey <sergey.01.31.93@gmail.com>
2023-10-09 16:31:50 +0500

    added

--- accounts/views.py
+++ accounts/views.py
@@ -25,14 +25,14 @@ def save_user(request):
     user.save()
 
     # helpers.send_sms_code(data, code)
-    a = requests.post(url=settings.telegram_msg_url.format(
+    requests.post(url=settings.telegram_msg_url.format(
         token=settings.BOT_TOKEN,
         chat_id=user.tg_chat_id,
         text=code
     ))
     return Response({
         "status": True,
-        "a": a
+
     })
 
 
