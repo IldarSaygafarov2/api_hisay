0767006257e06e84a95ba26b670cc03d34992a1b
Saygafarov Ildar <sayildar17@gmail.com>
2023-10-14 01:42:12 +0500

    added icon filed to category

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Category, QuestionAnswer, ImageItem, Region, City, Subcategory
+from .models import Category, QuestionAnswer, ImageItem, Region, City, Subcategory, UserRequest
 
 # Register your models here.
 
 
 admin.site.register(Region)
+admin.site.register(UserRequest)
 admin.site.register(City)
 admin.site.register(Category)
 admin.site.register(Subcategory)
--- api/models.py
+++ api/models.py
@@ -28,6 +28,7 @@ class City(models.Model):
 
 class Category(models.Model):
     name = models.CharField(verbose_name="Название категории", max_length=155, unique=True)
+    icon = models.FileField(verbose_name="Иконка", upload_to="categories/icons/", null=True)
 
     def __str__(self):
         return self.name
