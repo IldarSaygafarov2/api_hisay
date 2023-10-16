3040ce4c4e7e7345a55d17ff7a69117910e77557
Sergey <sergey.01.31.93@gmail.com>
2023-10-16 16:49:34 +0500

    added fixes

--- api/views.py
+++ api/views.py
@@ -11,6 +11,16 @@ class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
 
 
+# class CategoryRetrieveView(generics.RetrieveAPIView):
+#     queryset = Category.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         category = Category.objects.get(pk=kwargs['category_id'])
+#         user_requests = UserRequest.objects.filter(category=category)
+#         serializer = UserRequestSerializer(user_requests, many=True)
+#         return Response(serializer.data)
+
+
 class QuestionAnswerList(generics.ListAPIView):
     queryset = QuestionAnswer.objects.all()
     serializer_class = QuestionAnswerSerializer
@@ -25,10 +35,22 @@ class UserRequestCreateListView(generics.ListCreateAPIView):
     queryset = UserRequest.objects.all()
     serializer_class = UserRequestSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = UserRequest.objects.all()
+        ser = UserRequestSerializer(queryset, many=True)
+        return Response(ser.data)
+
 
 class UserRequestRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = UserRequest.objects.all()
     serializer_class = UserRequestSerializer
+
+    def get(self, request, *args, **kwargs):
+        queryset = UserRequest.objects.get(pk=kwargs['pk'])
+        ser = UserRequestSerializer(queryset, many=False)
+        return Response(ser.data)
+
+
 
 
 @api_view(["GET"])
@@ -38,5 +60,8 @@ def get_requests_by_category(request, category_id):
         return Response({"status": False})
 
     user_requests = UserRequest.objects.filter(category=category)
+
     serializer = UserRequestSerializer(user_requests, many=True)
+    print(request.META)
+    print(request.META['HTTP_HOST'])
     return Response(serializer.data)
